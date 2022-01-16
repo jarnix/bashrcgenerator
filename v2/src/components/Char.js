1a691d6562f27c7ece062ccd70070f520180b1cd
Julien Ricard <jarnix@jarnix.com>
2022-01-16 22:59:02 +0100

    wip

--- v2/src/components/Char.js
+++ v2/src/components/Char.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import { Label } from 'semantic-ui-react';
+import './Char.css';
 
 class Char extends React.Component {
+
     render() {
 
+        const styleForBorder = this.props.isDragging ? '1px dashed #4099ff' : '1px solid #ddd';
+
         return (
+
             <div
-              
-                >
-                    {this.props.displayText}
+                className="char"
+                style={styleForBorder}
+                ref={this.props.provided.innerRef}
+                {...this.props.provided.draggableProps}
+                {...this.props.provided.dragHandleProps}
+            >
+                {this.props.children}
             </div>
         );
     }
