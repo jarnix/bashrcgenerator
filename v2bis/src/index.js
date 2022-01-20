929f3187a1201bbd6f6da16e5f04adc95ae4bb7c
Julien Ricard <jarnix@jarnix.com>
2022-01-20 19:07:12 +0100

    hehe

---
+++ v2bis/src/index.js
@@ -0,0 +1,6 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+
+// Put the things into the DOM!
+ReactDOM.render(<App />, document.getElementById('root'));
